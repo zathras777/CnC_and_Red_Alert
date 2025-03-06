970600a813b35f9f9976bf5b52deababa31deaa7
Charlie Birks <charlie@daft.games>
2025-03-06 12:08:31 +0000

    Re-implement Apply_XOR_Delta

--- SDLLIB/wsa.cpp
+++ SDLLIB/wsa.cpp
@@ -26,6 +26,84 @@ int Get_Animation_Frame_Count(void *handle)
 
 unsigned int Apply_XOR_Delta(char *source_ptr, char *delta_ptr)
 {
-    printf("%s\n", __PRETTY_FUNCTION__);
+    auto udelta = (uint8_t *)delta_ptr;
+
+    // top_loop
+    while(true)
+    {
+        uint8_t b = *udelta++;
+
+        if(b == 0)
+        {
+            // SHORTRUN
+            int count = *udelta++; // get count
+            uint8_t xor_b = *udelta++; // get XOR byte
+            do
+            {
+                *source_ptr ^= xor_b; // XOR that byte
+                source_ptr++;
+            }
+            while(--count);
+        }
+        else if(b & 0x80)
+        {
+            // By now, we know it must be a LONGDUMP, SHORTSKIP, LONGRUN, or LONGSKIP
+            b &= 0x7F;
+            if(b == 0)
+            {
+                int count = udelta[0] | udelta[1] << 8; // get word code
+                udelta += 2;
+
+                if(!count)
+                    return 0; // long count of zero means stop
+
+                if(count & 0x8000)
+                {
+                    count &= 0x7FFF;
+                    if(count & 0x4000)
+                    {
+                        // LONGRUN
+                        uint8_t xor_b = *udelta++; // get XOR byte
+                        do
+                        {
+                            *source_ptr ^= xor_b; // XOR that byte
+                            source_ptr++;
+                        }
+                        while(--count);
+                    }
+                    else
+                    {
+                        // LONGDUMP
+                        do
+                        {
+                            uint8_t xor_b = *udelta++; // get delta XOR byte
+                            *source_ptr ^= xor_b; // xor that byte on the dest
+                            source_ptr++;
+                        }
+                        while(--count);
+                    }
+                }
+
+                // LONGSKIP
+                source_ptr += count;
+            }
+            else // SHORTSKIP
+                source_ptr += b;
+        }
+        else
+        {
+            // SHORTDUMP
+            int count = b;
+
+            do
+            {
+                uint8_t xor_b = *udelta++; // get delta XOR byte
+                *source_ptr ^= xor_b; // xor that byte on the dest
+                source_ptr++;
+            }
+            while(--count);
+        }
+    }
+
     return 0;
 }
