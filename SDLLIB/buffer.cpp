fcc2833383595565d3e9b9031ec5b9a7a16daa8a
Charlie Birks <charlie@daft.games>
2025-03-06 22:38:16 +0000

    Implement BufferClass
    So far the only problem this was causing was leaking memory on exit

--- SDLLIB/buffer.cpp
+++ SDLLIB/buffer.cpp
@@ -1,16 +1,17 @@
+#include <stdint.h>
+
 #include "buffer.h"
 
-BufferClass::BufferClass(long size)
+BufferClass::BufferClass(long size) : Buffer(new uint8_t[size]), Size(size), Allocated(true)
 {
-
 }
 
-BufferClass::BufferClass()
+BufferClass::BufferClass() : Buffer(NULL), Size(0), Allocated(false)
 {
-
 }
 
 BufferClass::~BufferClass()
 {
-    
+    if(Allocated)
+		delete[] (uint8_t *)Buffer;
 }
