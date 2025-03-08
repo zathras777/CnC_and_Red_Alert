c94ee5f1858e1d9b00ffc60158f4112748f0280f
Charlie Birks <charlie@daft.games>
2025-03-08 23:45:02 +0000

    Re-implement Apply_XOR_Delta_To_Page_Or_Viewport
    Almost entirely two duplicates of Apply_XOR_Delta

--- SDLLIB/wsa.cpp
+++ SDLLIB/wsa.cpp
@@ -108,3 +108,262 @@ unsigned int Apply_XOR_Delta(char *source_ptr, char *delta_ptr)
 
     return 0;
 }
+
+void Apply_XOR_Delta_To_Page_Or_Viewport(void *target, void *delta, int width, int nextrow, int copy)
+{
+    auto source_ptr = (uint8_t *)target;
+    auto udelta = (uint8_t *)delta;
+    
+    int x = 0;
+
+    auto ot = target;
+
+    if(copy == DO_XOR)
+    {
+        // mostly duplicated from Apply_XOR_Delta
+        // except for the row checks
+        while(true)
+        {
+            uint8_t b = *udelta++;
+    
+            if(b == 0)
+            {
+                // SHORTRUN
+                int count = *udelta++; // get count
+                uint8_t xor_b = *udelta++; // get XOR byte
+                do
+                {
+                    *source_ptr ^= xor_b; // XOR that byte
+                    source_ptr++;
+
+                    if(++x == width)
+                    {
+                        // final column, go to next row
+                        x = 0;
+                        source_ptr = source_ptr - width + nextrow;
+                    }
+                }
+                while(--count);
+            }
+            else if(b & 0x80)
+            {
+                // By now, we know it must be a LONGDUMP, SHORTSKIP, LONGRUN, or LONGSKIP
+                b &= 0x7F;
+                if(b == 0)
+                {
+                    int count = udelta[0] | udelta[1] << 8; // get word code
+                    udelta += 2;
+    
+                    if(!count)
+                        return; // long count of zero means stop
+    
+                    if(count & 0x8000)
+                    {
+                        if(count & 0x4000)
+                        {
+                            // LONGRUN
+                            count &= 0x3FFF;
+                            uint8_t xor_b = *udelta++; // get XOR byte
+                            do
+                            {
+                                *source_ptr ^= xor_b; // XOR that byte
+                                source_ptr++;
+
+                                if(++x == width)
+                                {
+                                    // final column, go to next row
+                                    x = 0;
+                                    source_ptr = source_ptr - width + nextrow;
+                                }
+                            }
+                            while(--count);
+                        }
+                        else
+                        {
+                            // LONGDUMP
+                            count &= 0x7FFF;
+                            do
+                            {
+                                uint8_t xor_b = *udelta++; // get delta XOR byte
+                                *source_ptr ^= xor_b; // xor that byte on the dest
+                                source_ptr++;
+
+                                if(++x == width)
+                                {
+                                    // final column, go to next row
+                                    x = 0;
+                                    source_ptr = source_ptr - width + nextrow;
+                                }
+                            }
+                            while(--count);
+                        }
+                    }
+    
+                    // LONGSKIP
+                    source_ptr -= x; // go back to beginning or row.
+                    x += count;
+                    while(x >= width)
+                    {
+                        x -= width;
+                        source_ptr += nextrow;
+                    }
+                    source_ptr += x; // get to correct position in row.
+                }
+                else // SHORTSKIP
+                {
+                    source_ptr -= x; // go back to beginning or row.
+                    x += b;
+                    while(x >= width)
+                    {
+                        x -= width;
+                        source_ptr += nextrow;
+                    }
+                    source_ptr += x; // get to correct position in row.
+                }
+            }
+            else
+            {
+                // SHORTDUMP
+                int count = b;
+    
+                do
+                {
+                    uint8_t xor_b = *udelta++; // get delta XOR byte
+                    *source_ptr ^= xor_b; // xor that byte on the dest
+                    source_ptr++;
+                    if(++x == width)
+                    {
+                        // final column, go to next row
+                        x = 0;
+                        source_ptr = source_ptr - width + nextrow;
+                    }
+                }
+                while(--count);
+            }
+        }
+    }
+    else // copy
+    {
+        // same thing without xor...
+        while(true)
+        {
+            uint8_t b = *udelta++;
+    
+            if(b == 0)
+            {
+                // SHORTRUN
+                int count = *udelta++; // get count
+                uint8_t xor_b = *udelta++; // get byte
+                do
+                {
+                    *source_ptr = xor_b; // store that byte
+                    source_ptr++;
+
+                    if(++x == width)
+                    {
+                        // final column, go to next row
+                        x = 0;
+                        source_ptr = source_ptr - width + nextrow;
+                    }
+                }
+                while(--count);
+            }
+            else if(b & 0x80)
+            {
+                // By now, we know it must be a LONGDUMP, SHORTSKIP, LONGRUN, or LONGSKIP
+                b &= 0x7F;
+                if(b == 0)
+                {
+                    int count = udelta[0] | udelta[1] << 8; // get word code
+                    udelta += 2;
+    
+                    if(!count)
+                        return; // long count of zero means stop
+
+                    if(count & 0x8000)
+                    {
+                        if(count & 0x4000)
+                        {
+                            // LONGRUN
+                            count &= 0x3FFF;
+                            uint8_t xor_b = *udelta++; // get byte
+                            do
+                            {
+                                *source_ptr = xor_b; // store that byte
+                                source_ptr++;
+
+                                if(++x == width)
+                                {
+                                    // final column, go to next row
+                                    x = 0;
+                                    source_ptr = source_ptr - width + nextrow;
+                                }
+                            }
+                            while(--count);
+                        }
+                        else
+                        {
+                            // LONGDUMP
+                            count &= 0x7FFF;
+                            do
+                            {
+                                uint8_t xor_b = *udelta++; // get delta byte
+                                *source_ptr = xor_b; // store that byte
+                                source_ptr++;
+
+                                if(++x == width)
+                                {
+                                    // final column, go to next row
+                                    x = 0;
+                                    source_ptr = source_ptr - width + nextrow;
+                                }
+                            }
+                            while(--count);
+                        }
+                    }
+    
+                    // LONGSKIP
+                    source_ptr -= x; // go back to beginning or row.
+                    x += count;
+                    while(x >= width)
+                    {
+                        x -= width;
+                        source_ptr += nextrow;
+                    }
+                    source_ptr += x; // get to correct position in row.
+                }
+                else // SHORTSKIP
+                {
+                    source_ptr -= x; // go back to beginning or row.
+                    x += b;
+                    while(x >= width)
+                    {
+                        x -= width;
+                        source_ptr += nextrow;
+                    }
+                    source_ptr += x; // get to correct position in row.
+                }
+            }
+            else
+            {
+                // SHORTDUMP
+                int count = b;
+    
+                do
+                {
+                    uint8_t xor_b = *udelta++; // get delta byte
+                    *source_ptr = xor_b; // store that byte
+                    source_ptr++;
+                    if(++x == width)
+                    {
+                        // final column, go to next row
+                        x = 0;
+                        source_ptr = source_ptr - width + nextrow;
+                    }
+                }
+                while(--count);
+            }
+        }
+    }
+}
+
