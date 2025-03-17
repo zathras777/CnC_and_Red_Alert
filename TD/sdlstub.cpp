6db80fe6ef790660585b4c2c2d7cef5d771952e6
Charlie Birks <charlie@daft.games>
2025-03-17 19:25:46 +0000

    TD: re-implement Shake_Screen
    Steal the code from RA

--- TD/sdlstub.cpp
+++ TD/sdlstub.cpp
@@ -220,8 +220,39 @@ extern "C" long Calculate_CRC(void *buffer, long length)
 }
 
 // SHAKESCR.ASM in WIN32LIB
-// RA uses Shake_The_Screen in conquer.cpp instead
-void Shake_Screen(int)
+// based on Shake_The_Screen in RA's conquer.cpp
+void Shake_Screen(int shakes)
 {
-	printf("Shake_Screen\n");
+	shakes += shakes;
+
+	Hide_Mouse();
+	SeenBuff.Blit(HidPage);
+	int oldyoff = 0;
+	int newyoff = 0;
+	while(shakes--) {
+		int x = TickCount.Time();
+
+		do {
+			newyoff = Sim_Random_Pick(0,2) - 1;
+		} while (newyoff == oldyoff);
+		switch (newyoff) {
+			case -1:
+				HidPage.Blit(SeenBuff, 0,2, 0,0, 640,398);
+				break;
+			case 0:
+				HidPage.Blit(SeenBuff);
+				break;
+			case 1:
+				HidPage.Blit(SeenBuff, 0,0, 0,2, 640,398);
+				break;
+		}
+#ifdef PORTABLE
+		while (x == TickCount.Time()) Video_End_Frame();
+#else
+		while (x == TickCount);
+#endif
+	}
+
+	HidPage.Blit(SeenBuff);
+	Show_Mouse();
 }
